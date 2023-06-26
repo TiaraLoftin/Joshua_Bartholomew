86fac1286f5197e4b229f022c96594ae66806b49
TiaraLoftin <tloftin68@gmail.com>
2023-06-26 14:09:11 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_i3vwd2s";
+  const templateID = "template_fc5cmnu";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
