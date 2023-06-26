dc7dba1d75f426bb53d9118e07fac7825d9f5e41
TiaraLoftin <tloftin68@gmail.com>
2023-06-26 15:15:13 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,13 +1,10 @@
-function send()
-{
-  emailjs.send("service_i3vwd2s","template_fc5cmnu",{
-    from_name: document.querySelector("#from_name").value,
-    email_to: document.querySelector("#email").value,
-    message: document.querySelector("#message").value,
-  }).then(response =>{
-    console.log("success", response.status);
-  },(error)=>{
-    console.log(error);
+function sendMail(){
+  var params ={
+    from_name : document.getElementById("from_name").value,
+    email: document.getElementById("email").value,
+    message : document.getElementById("message").value
+  }
+  emailjs.send("service_i3vwd2s", "template_fc5cmnu", params).then(function (res){
+    alert("Success!" +res.status);
   })
 }
-document.querySelector(".send").addEventListener("click",send)
