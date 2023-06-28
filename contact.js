9ea7210b3d382834078dac436f3c5c1a4c39330a
TiaraLoftin <tloftin68@gmail.com>
2023-06-28 13:54:07 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,10 +1,18 @@
-function sendMail(){
-  var params ={
-    from_name : document.getElementById("from_name").value,
-    email: document.getElementById("email").value,
-    message : document.getElementById("message").value
-  }
-  emailjs.send("service_2hy6l8j", "template_fc5cmnu", params).then(function (res){
-    alert("Success!" +res.status);
-  })
+/* Sending Email from Contact Section */
+(function () {
+    emailjs.init("user_oh1B3JnEWlyp0zwpb");
+})();
+
+function sendmail() {
+    let fullName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userMessage = document.getElementById("message").value;
+
+        var contactParams = {
+            from_name: fullName,
+            email_to: userEmail,
+            message: userMessage
+        };
+
+        emailjs.send('service_2hy6l8j', 'template_fc5cmnu', contactParams).then(function (res) {alert("Success!" +res.status);})
 }
